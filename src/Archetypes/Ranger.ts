71508cbe1fc8cc7357a78ff08ffe762da6ff7189
Bruno Krauthamer <brunokrauthamer@gmail.com>
2022-11-22 18:13:20 -0300

    req05

---
+++ src/Archetypes/Ranger.ts
@@ -0,0 +1,23 @@
+import { EnergyType } from '../Energy';
+import Archetypes from './Archetype';
+
+class Ranger extends Archetypes {
+  private static _instancesNumber = 0;
+  private _energyType: EnergyType;
+  
+  constructor(name: string) {
+    super(name);
+    this._energyType = 'stamina';
+  }
+
+  static createdArchetypeInstances(): number {
+    this._instancesNumber += 1;
+    return this._instancesNumber;
+  }
+
+  get energyType(): EnergyType {
+    return this._energyType;
+  }
+}
+
+export default Ranger;
